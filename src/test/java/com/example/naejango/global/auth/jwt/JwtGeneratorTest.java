72a1d2df9e88d10416b704c0ac77077c6c9c9b80
utopiandreams <berserkppp@naver.com>
2023-06-23 22:58:40 +0900

    Test : JwtValidatorTest 추가 등 - JwtValidatorTest 추가 - Jwt 관련 test @Tag("jwt") 추가

--- src/test/java/com/example/naejango/global/auth/jwt/JwtGeneratorTest.java
+++ src/test/java/com/example/naejango/global/auth/jwt/JwtGeneratorTest.java
@@ -4,10 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.naejango.domain.user.domain.User;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,9 +12,9 @@ import java.time.Instant;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Tag("jwt")
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@DisplayName("JwtGenerator가")
 class JwtGeneratorTest {
     @Autowired
     private JwtGenerator jwtGenerator;
@@ -25,30 +22,25 @@ class JwtGeneratorTest {
     private String refreshToken;
     private User testUser;
     @Test
-    @DisplayName("JWT를 생성")
+    @DisplayName("JwtGenerator가 JWT를 생성")
     void generateJWT(){
         testUser = User.builder().userKey("test").build();
         accessToken = jwtGenerator.generateAccessToken(testUser);
         refreshToken = jwtGenerator.generateRefreshToken(testUser);
-
-        System.out.println("accessToken = " + accessToken);
-        System.out.println("refreshToken = " + refreshToken);
     }
 
     @Nested
     @DisplayName("생성된 JWT는")
     class WhenGeneratedJWT {
         @Test
-        @DisplayName("Null이 아님")
+        @DisplayName("null이 아님")
         void isNotNull() {
-            System.out.println("accessToken = " + accessToken);
-            System.out.println("refreshToken = " + refreshToken);
             assertNotNull(accessToken);
             assertNotNull(refreshToken);
         }
 
         @Test
-        @DisplayName("Prefix가 정상임")
+        @DisplayName("Prefix가 정상")
         void isProperPrefix() {
             assertTrue(accessToken.startsWith(JwtProperties.ACCESS_TOKEN_PREFIX));
             assertTrue(refreshToken.startsWith(JwtProperties.REFRESH_TOKEN_PREFIX));
@@ -64,7 +56,7 @@ class JwtGeneratorTest {
             assertEquals(testUser.getUserKey(), decodedRefreshToken.getClaim("userKey").asString());
         }
         @Test
-        @DisplayName("유효기간이 정상임")
+        @DisplayName("유효기간이 정상")
         void expiration(){
             DecodedJWT decodedAccessToken = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build().verify(accessToken.replace(JwtProperties.ACCESS_TOKEN_PREFIX, ""));
             DecodedJWT decodedRefreshToken = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build().verify(refreshToken.replace(JwtProperties.REFRESH_TOKEN_PREFIX, ""));
