25ffb9ff487e44d9c81cc37df731896418db344c
sejun An <asjjun@naver.com>
2023-07-14 07:41:22 +0900

    fix: Gender 수정
    - Json 파싱을 위한 애너테이션 추가

--- src/main/java/com/example/naejango/domain/user/domain/Gender.java
+++ src/main/java/com/example/naejango/domain/user/domain/Gender.java
@@ -1,5 +1,10 @@
 package com.example.naejango.domain.user.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum Gender {
-    Male, Female
+    @JsonProperty("남")
+    Male,
+    @JsonProperty("여")
+    Female
 }
