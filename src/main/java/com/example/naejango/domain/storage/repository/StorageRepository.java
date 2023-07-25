5a735940d4a300de23b155e595e9ad82d4d4c0b7
utopiandreams <berserkppp@naver.com>
2023-07-26 05:08:02 +0900

    feat: findNearbyStorage 메서드 추가
    - 좌표, 반경 기준으로 Storage 조회하는 메서드 추가

--- src/main/java/com/example/naejango/domain/storage/repository/StorageRepository.java
+++ src/main/java/com/example/naejango/domain/storage/repository/StorageRepository.java
@@ -1,7 +1,10 @@
 package com.example.naejango.domain.storage.repository;
 
 import com.example.naejango.domain.storage.domain.Storage;
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +12,14 @@ import java.util.List;
 @Repository
 public interface StorageRepository extends JpaRepository<Storage, Long> {
 
-    /** 창고를 등록한 유저를 기준으로 조회 */
+    /**
+     * 창고를 등록한 유저를 기준으로 조회
+     */
     List<Storage> findByUserId(Long userId);
+
+    /**
+     * 특정 Point 및 반경을 기준으로 조회
+     */
+    @Query("SELECT s FROM Storage s WHERE FUNCTION('ST_DWithin', :point, s.location, :radius, false) = true")
+    List<Storage> findNearbyStorage(@Param("point") Point point, @Param("radius") int radius);
 }
--- src/test/java/com/example/naejango/domain/storage/repository/StorageRepositoryTest.java
+++ src/test/java/com/example/naejango/domain/storage/repository/StorageRepositoryTest.java
@@ -4,20 +4,24 @@ import com.example.naejango.domain.storage.domain.Storage;
 import com.example.naejango.domain.user.domain.Role;
 import com.example.naejango.domain.user.domain.User;
 import com.example.naejango.domain.user.repository.UserRepository;
+import com.example.naejango.global.common.handler.GeomUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.PrecisionModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 @DataJpaTest
@@ -28,7 +32,8 @@ class StorageRepositoryTest {
     private StorageRepository storageRepository;
     @Autowired
     private UserRepository userRepository;
-    private final GeometryFactory factory = new GeometryFactory();
+    private final GeometryFactory factory = new GeometryFactory(new PrecisionModel(), 4326);
+    private final GeomUtil geomUtil = new GeomUtil();
 
     @Test
     @DisplayName("save: 창고 생성")
@@ -46,7 +51,7 @@ class StorageRepositoryTest {
                 .imgUrl("Test Url")
                 .address("Test Address")
                 .description("This is for a test")
-                .location(factory.createPoint(new Coordinate(123.123, 456.456)))
+                .location(factory.createPoint(new Coordinate(12.12, 34.34)))
                 .user(testUser)
                 .build();
 
@@ -59,8 +64,49 @@ class StorageRepositoryTest {
 
         // then
         assertEquals(findStorage, testStorage);
-        assertEquals(findStorage.getLocation().getX(), 123.123);
-        assertEquals(findStorage.getLocation().getY(), 456.456);
+        assertEquals(findStorage.getLocation().getX(), 12.12, 34.34);
+        assertEquals(findStorage.getLocation().getY(), 12.12, 34.34);
+    }
+
+    @Test
+    @Rollback(value = false)
+    @DisplayName("findNearbyStorage: 좌표 기준으로 근처 창고 조회")
+    public void findNearbyStorage() {
+        // given
+        Storage testStorage1 = Storage.builder()
+                .name("남산타워").address("")
+                .location(factory.createPoint(new Coordinate(126.98820, 37.55126)))
+                .build();
+
+        Storage testStorage2 = Storage.builder()
+                .name("경복궁").address("")
+                .location(factory.createPoint(new Coordinate(126.97689, 37.57760)))
+                .build();
+
+
+        Storage testStorage3 = Storage.builder()
+                .name("광화문 이순신 장군").address("")
+                .location(factory.createPoint(new Coordinate(126.97700, 37.57098)))
+                .build();
+
+        int d12 = geomUtil.calculateDistance(testStorage1.getLocation(), testStorage2.getLocation());
+        int d23 = geomUtil.calculateDistance(testStorage2.getLocation(), testStorage3.getLocation());
+        int d31 = geomUtil.calculateDistance(testStorage1.getLocation(), testStorage3.getLocation());
+
+        System.out.println("d12(남산타워 ~ 경복궁) = " + d12); // 3093 m
+        System.out.println("d23(경복궁 ~ 이순신장군) = " + d23); // 736 m
+        System.out.println("d31(이순신장군 ~ 남산타워)= " + d31); // 2405 m
+
+        storageRepository.save(testStorage1);
+        storageRepository.save(testStorage2);
+        storageRepository.save(testStorage3);
+        // when
+        List<Storage> result1 = storageRepository.findNearbyStorage(testStorage2.getLocation(), 2000);
+        List<Storage> result2 = storageRepository.findNearbyStorage(testStorage1.getLocation(), 2500);
+
+        // then
+        assertTrue(result1.contains(testStorage3) && !result1.contains(testStorage1));
+        assertTrue(result2.contains(testStorage3) && !result2.contains(testStorage2));
     }
 
     @Test
