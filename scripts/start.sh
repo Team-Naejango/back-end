2880025b78b7165ae15f2c4d15540e553967caa6
sejun Ahn <asjjun@naver.com>
2023-07-31 17:40:35 +0900

    chore: Shell Script 수정
    PID 인식 못하는 문제 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,7 @@
 
 PROJECT_ROOT="/home/ec2-user/action"
 JAR_FILE="$PROJECT_ROOT/build/libs/naejango-0.0.1-SNAPSHOT.jar"
+APPLICATION_NAME="naejango-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -13,5 +14,6 @@ TIME_NOW=$(date +%c)
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+# 현재 구동 중인 애플리케이션 pid 확인
+CURRENT_PID=$(pgrep -f $APPLICATION_NAME)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,13 +2,13 @@
 
 PROJECT_ROOT="/home/ec2-user/action"
 JAR_FILE="$PROJECT_ROOT/build/libs/naejango-0.0.1-SNAPSHOT.jar"
-
+APPLICATION_NAME="naejango-0.0.1-SNAPSHOT.jar"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
 # 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -f $APPLICATION_NAME)
 
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
