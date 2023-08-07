fa1b0e3b9dcc913b7c15ab00be47f1ec2e54a95d
utopiandreams <berserkppp@naver.com>
2023-08-07 20:47:04 +0900

    fix: Security 버그 수정 및 리팩터링
    - Guest Role 추가하여 RefreshToken 의 인증절차 수정
    - Jwt 프론트 전달 방식을 cookie 로 수정
    - SecurityConfig 수정 (antMatcher 수정)
    - 람다 표현 도입 등

--- src/main/java/com/example/naejango/global/auth/handler/AccessDeniedHandlerImpl.java
+++ src/main/java/com/example/naejango/global/auth/handler/AccessDeniedHandlerImpl.java
@@ -2,7 +2,8 @@ package com.example.naejango.global.auth.handler;
 
 import com.example.naejango.domain.user.domain.Role;
 import com.example.naejango.domain.user.domain.User;
-import com.example.naejango.global.auth.principal.PrincipalDetails;
+import com.example.naejango.global.common.handler.CommonDtoHandler;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
@@ -18,9 +19,14 @@ import java.io.IOException;
  * 권한이 없는 요청(403 ERROR)을 처리하는 핸들러
  */
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
-    private static final String temporalUserRedirectUrl = "/"; // 임시
+
+    private final CommonDtoHandler commonDtoHandler;
+
+    private final String loginPage = "/";
+    private final String joinPage = "/"; // 임시
 
     /**
      * 권한이 없는 요청은 전부 Custom 403 페이지로 redirect
@@ -28,16 +34,10 @@ public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println("authentication = " + authentication);
-        if (authentication != null) {
-            PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
-            User user = principalDetails.getUser();
-            if (user.getRole() == Role.TEMPORAL) {
-                response.sendRedirect(temporalUserRedirectUrl);
-                return;
-            }
-            log.warn("권한이 없는 요청 : User({}), RequestUrl({})",user.getId() ,request.getRequestURI());
-        }
-        response.sendRedirect("/");
+        User user = commonDtoHandler.userFromAuthentication(authentication);
+        // 로그인 되지 않은 회원의 접근
+        if (user == null) response.sendRedirect(loginPage);
+        // Temporal 회원의 접근
+        if (user.getRole() == Role.TEMPORAL) response.sendRedirect(joinPage);
     }
 }
--- src/main/java/com/example/naejango/global/auth/jwt/JwtProperties.java
+++ src/main/java/com/example/naejango/global/auth/jwt/JwtProperties.java
@@ -8,24 +8,27 @@ import javax.annotation.PostConstruct;
 public class JwtProperties {
     @Value("${jwt.secret}")
     private String secret;
+    @Value("${jwt.access-token.cookieName}")
+    private String accessTokenCookieName;
     @Value("${jwt.access-token.header}")
     private String accessTokenHeader;
     @Value("${jwt.access-token.prefix}")
     private String accessTokenPrefix;
     @Value("${jwt.access-token.expiration-length}")
     private long accessTokenExpirationLength;
-    @Value("${jwt.refresh-token.header}")
-    private String refreshTokenCookie;
+    @Value("${jwt.refresh-token.cookieName}")
+    private String refreshTokenCookieName;
     @Value("${jwt.refresh-token.expiration-length}")
     private long refreshTokenExpirationLength;
     @Value("${jwt.iss}")
     private String iss;
 
     public static String SECRET;
+    public static String ACCESS_TOKEN_COOKIE_NAME;
     public static String ACCESS_TOKEN_HEADER;
     public static String ACCESS_TOKEN_PREFIX;
     public static long ACCESS_TOKEN_EXPIRATION_TIME;
-    public static String REFRESH_TOKEN_COOKIE;
+    public static String REFRESH_TOKEN_COOKIE_NAME;
     public static long REFRESH_TOKEN_EXPIRATION_TIME;
     public static String ISS;
 
@@ -33,11 +36,12 @@ public class JwtProperties {
     public void init(){
         SECRET = secret;
 
+        ACCESS_TOKEN_COOKIE_NAME = accessTokenCookieName;
         ACCESS_TOKEN_HEADER = accessTokenHeader;
         ACCESS_TOKEN_PREFIX = accessTokenPrefix;
         ACCESS_TOKEN_EXPIRATION_TIME = accessTokenExpirationLength;
 
-        REFRESH_TOKEN_COOKIE = refreshTokenCookie;
+        REFRESH_TOKEN_COOKIE_NAME = refreshTokenCookieName;
         REFRESH_TOKEN_EXPIRATION_TIME = refreshTokenExpirationLength;
         ISS = iss;
     }
