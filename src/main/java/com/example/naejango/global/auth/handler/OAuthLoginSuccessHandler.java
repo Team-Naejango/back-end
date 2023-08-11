f4e3c8278b34aa77d4fabc6fe7922ebf1c5861e2
utopiandreams <berserkppp@naver.com>
2023-08-11 18:12:48 +0900

    fix: Security 버그 수정
    - cookie 접근허용 범위 수정

--- src/main/java/com/example/naejango/global/auth/handler/OAuthLoginSuccessHandler.java
+++ src/main/java/com/example/naejango/global/auth/handler/OAuthLoginSuccessHandler.java
@@ -24,6 +24,8 @@ public class OAuthLoginSuccessHandler implements AuthenticationSuccessHandler {
     private final JwtGenerator jwtGenerator;
     private final CommonDtoHandler commonDtoHandler;
     private final String redirectUrl = "http://localhost:3000/oauth/kakaoCallback";
+    private final String localFrontDomain = "http://localhost:3000";
+    private final String FrontDomain = "https://d1ad0vl3i2dudp.cloudfront.net";
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
@@ -46,12 +48,15 @@ public class OAuthLoginSuccessHandler implements AuthenticationSuccessHandler {
         userService.refreshSignature(userId, refreshToken);
 
         Cookie accessTokenCookie = new Cookie(JwtProperties.ACCESS_TOKEN_COOKIE_NAME, accessToken);
-        Cookie refreshTokenCookie = new Cookie(JwtProperties.REFRESH_TOKEN_COOKIE_NAME, refreshToken);
-        accessTokenCookie.setHttpOnly(true);
-        accessTokenCookie.setPath("/");
-        refreshTokenCookie.setHttpOnly(true);
-        refreshTokenCookie.setPath("/");
+        Cookie refreshTokenCookie1 = new Cookie(JwtProperties.REFRESH_TOKEN_COOKIE_NAME, refreshToken);
+        Cookie refreshTokenCookie2 = new Cookie(JwtProperties.REFRESH_TOKEN_COOKIE_NAME, refreshToken);
+        accessTokenCookie.setHttpOnly(false); // 자바스크립트 접근 허용
+        refreshTokenCookie1.setHttpOnly(true);
+        refreshTokenCookie1.setPath(localFrontDomain);
+        refreshTokenCookie2.setHttpOnly(true);
+        refreshTokenCookie2.setPath(FrontDomain);
         response.addCookie(accessTokenCookie);
-        response.addCookie(refreshTokenCookie);
+        response.addCookie(refreshTokenCookie1);
+        response.addCookie(refreshTokenCookie2);
     }
 }
