062f6c13e0d5675279be03e495f3cd17972597ce
utopiandreams <berserkppp@naver.com>
2023-09-08 11:25:54 +0900

    fix: Exception Handling 수정
    - Binding 시 익셉션 핸들링 수정

--- src/main/java/com/example/naejango/global/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/naejango/global/common/exception/GlobalExceptionHandler.java
@@ -1,20 +1,20 @@
 package com.example.naejango.global.common.exception;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value = CustomException.class)
+    @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> CustomExceptionHandler(CustomException exception) {
         return ErrorResponse.toResponseEntity(exception.getErrorCode());
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidationResponse> ValidationExceptionHandler(MethodArgumentNotValidException e) {
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ValidationResponse> ValidationExceptionHandler(BindException e) {
         return ValidationResponse.toResponseEntity(e);
     }
 
