5a50a05d4f7e9625247ecf030251bef5f137ebbb
sejun Ahn <asjjun@naver.com>
2023-09-18 06:01:40 +0900

    refactor: Transaction 조회 쿼리 수정

--- src/main/java/com/example/naejango/domain/transaction/repository/TransactionRepository.java
+++ src/main/java/com/example/naejango/domain/transaction/repository/TransactionRepository.java
@@ -14,9 +14,9 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
 
     @Query("SELECT t from Transaction t " +
             "left join fetch t.item " +
-            "left join fetch t.user.userProfile " +
-            "left join fetch t.trader.userProfile")
-    List<Transaction> findByUserIdOrTraderId(Long userId, Long traderId);
+            "left join fetch UserProfile up on up.id = t.trader.id or up.id = t.user.id " +
+            "where t.user.id = :userId or t.trader.id = :traderId")
+    List<Transaction> findByUserIdOrTraderId(@Param("userId") Long userId, @Param("traderId") Long traderId);
 
     @Modifying
     @Query("UPDATE Transaction t SET t.item = null WHERE t.item.id = :itemId")
