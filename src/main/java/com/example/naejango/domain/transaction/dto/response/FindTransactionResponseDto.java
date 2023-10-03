993b90d8d2cd60a21354e2c8fe750bbe5e807d1f
sejun Ahn <asjjun@naver.com>
2023-10-04 04:49:14 +0900

    refactor: 거래 정보 응답 dto 수정
    - 정보 요청한 유저가 구매자 일 경우 판매 금액 컬럼을 음수로 응답 하도록 수정

--- src/main/java/com/example/naejango/domain/transaction/dto/response/FindTransactionResponseDto.java
+++ src/main/java/com/example/naejango/domain/transaction/dto/response/FindTransactionResponseDto.java
@@ -15,7 +15,7 @@ public class FindTransactionResponseDto {
 
     private String date;
 
-    private int amount;
+    private int amount; // 판매는 거래 금액 그대로, 구매는 거래 금액의 -로 응답
 
     private String status; // 구매 or 판매
 
@@ -28,12 +28,13 @@ public class FindTransactionResponseDto {
     public FindTransactionResponseDto(Transaction transaction, Long userId) {
         this.id = transaction.getId();
         this.date = transaction.getDate().toString();
-        this.amount = transaction.getAmount();
 
         if (transaction.getUser().getId().equals(userId)) {
+            this.amount = transaction.getAmount();
             this.status = "판매";
             this.traderName = transaction.getTrader().getUserProfile().getNickname();
         } else if (transaction.getTrader().getId().equals(userId)) {
+            this.amount = -1 * transaction.getAmount();
             this.status = "구매";
             this.traderName = transaction.getUser().getUserProfile().getNickname();
         }
