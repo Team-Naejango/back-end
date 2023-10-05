bc250e2bc5ea5f108e732c1669302d0afa0e6bfb
utopiandreams <berserkppp@naver.com>
2023-10-05 15:05:12 +0900

    feat: 채팅 목록 조회시 itemId 포함

--- src/main/java/com/example/naejango/domain/chat/dto/ChatInfoDto.java
+++ src/main/java/com/example/naejango/domain/chat/dto/ChatInfoDto.java
@@ -15,23 +15,25 @@ import java.time.LocalDateTime;
 @ToString
 public class ChatInfoDto {
     private Long channelId;
-    private Long chatId;
     private ChannelType channelType;
-    private String title;
-    private long unreadCount;
     private int participantsCount;
     private int channelLimit;
+    private Long itemId;
+    private Long chatId;
+    private String title;
+    private long unreadCount;
     private String lastMessage;
     private LocalDateTime lastChatAt;
 
     public ChatInfoDto(Chat chat, Channel channel, long unreadCount) {
         this.channelId = chat.getChannel().getId();
-        this.chatId = chat.getId();
         this.channelType = chat.getChannel().getChannelType();
+        this.participantsCount = channel.getChannelType().equals(ChannelType.GROUP)? ((GroupChannel)channel).getParticipantsCount() : 0;
+        this.channelLimit = channel.getChannelType().equals(ChannelType.GROUP)? ((GroupChannel)channel).getChannelLimit() : 0;
+        this.itemId = channel.getChannelType().equals(ChannelType.GROUP)? ((GroupChannel)channel).getItem().getId() : 0L;
+        this.chatId = chat.getId();
         this.title = chat.getTitle();
         this.unreadCount = unreadCount;
-        this.participantsCount = channel.getChannelType().equals(ChannelType.GROUP)? ((GroupChannel)channel).getParticipantsCount() : 2;
-        this.channelLimit = channel.getChannelType().equals(ChannelType.GROUP)? ((GroupChannel)channel).getChannelLimit() : 2;
         this.lastMessage = channel.getLastMessage();
         this.lastChatAt = channel.getLastModifiedDate();
     }
