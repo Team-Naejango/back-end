d6ac8cdf23b888058322dbfc2847912a15b23669
utopiandreams <berserkppp@naver.com>
2023-10-15 14:39:24 +0900

    fix: 근처 storage 검색시 N+1 문제해결(queryDsl 도입)

--- src/main/java/com/example/naejango/domain/storage/dto/StorageAndDistanceDto.java
+++ src/main/java/com/example/naejango/domain/storage/dto/StorageAndDistanceDto.java
@@ -1,21 +1,14 @@
 package com.example.naejango.domain.storage.dto;
 
 import com.example.naejango.domain.storage.domain.Storage;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @ToString
 public class StorageAndDistanceDto {
     private Storage storage;
-    private double distance;
-
-    public StorageAndDistanceDto(Storage storage, double distance) {
-        this.storage = storage;
-        this.distance = distance;
-    }
+    private Integer distance;
 }
