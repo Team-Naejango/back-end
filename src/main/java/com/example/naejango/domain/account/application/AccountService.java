a3141dfecda9f297b40e8070fcdd7e198cb6bc91
sejun Ahn <asjjun@naver.com>
2023-11-06 01:02:51 +0900

    style: 사용 안하는 코드 삭제

--- src/main/java/com/example/naejango/domain/account/application/AccountService.java
+++ src/main/java/com/example/naejango/domain/account/application/AccountService.java
@@ -2,32 +2,17 @@ package com.example.naejango.domain.account.application;
 
 import com.example.naejango.domain.account.domain.Account;
 import com.example.naejango.domain.account.repository.AccountRepository;
-import com.example.naejango.domain.user.domain.User;
 import com.example.naejango.global.common.exception.CustomException;
 import com.example.naejango.global.common.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AccountService {
     private final AccountRepository accountRepository;
-    private final EntityManager em;
-
-    /**
-     * 계좌 생성
-     * 유저 프로필 등록시 호출됨
-     */
-    @Transactional
-    public void createAccount(Long userId){
-        Account account = Account.builder().user(em.getReference(User.class, userId)).balance(0).build();
-
-        accountRepository.save(account);
-    }
 
     /** 계좌에 금액 충전 */
     @Transactional
