6876798f73cc7c6108fe607623a532dda90d87af
sejun Ahn <asjjun@naver.com>
2023-11-13 00:57:33 +0900

    fix: 표준 출력 및 표준 에러 출력 파일 지정
    - AWS CodeDeploy에서 배포 실패로 뜨던것 수정
    - 배포 프로세스는 제대로 동작 했지만 출력을 지정 하지 않아서 에러가 발생
    했던 것

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,12 +4,14 @@ PROJECT_ROOT="/home/ec2-user/action"
 JAR_FILE="$PROJECT_ROOT/build/libs/naejango-0.0.1-SNAPSHOT.jar"
 APPLICATION_NAME="naejango-0.0.1-SNAPSHOT.jar"
 DEPLOY_LOG="$PROJECT_ROOT/log/deploy.log"
+APP_LOG="$PROJECT_ROOT/log/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
 
 TIME_NOW=$(date +%c)
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE &
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 # 현재 구동 중인 애플리케이션 pid 확인
 CURRENT_PID=$(pgrep -f $APPLICATION_NAME)
